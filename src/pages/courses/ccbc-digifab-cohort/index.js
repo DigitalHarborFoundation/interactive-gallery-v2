6fc0a0414b16d4ad55b79965866385d0e679d2fd
jonathanprozzi <jonathanprozzi@gmail.com>
2019-05-09 16:20:08 -0400

    all course pages now render filtered entries based on the course route -- theres definitely a more modular way to do this but i wanted to get it working first

--- src/pages/courses/ccbc-digifab-cohort/index.js
+++ src/pages/courses/ccbc-digifab-cohort/index.js
@@ -1,10 +1,51 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { graphql, StaticQuery } from 'gatsby';
+import ProjectCard from '../../../../src/components/project-card';
+import {
+  ContentWrapper,
+  GridThreeColumnContainer,
+} from '../../../components/elements/layout-components';
 
-const CCBCDigifabCohort = () => (
-  <>
-    <h1>CCBC DIGIFAB COHORT LANDING PAGE</h1>
-  </>
+const CCBC_QUERY = graphql`
+  query CCBCQuery {
+    CCBCEntry: allMarkdownRemark(
+      filter: { frontmatter: { course: { eq: "ccbc-digifab-cohort" } } }
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            youth
+            semester
+          }
+        }
+      }
+    }
+  }
+`;
+
+// TODO: Make the project list more accessible by adding ul and li elements
+
+const CCBCPage = () => (
+  <StaticQuery
+    query={CCBC_QUERY}
+    render={({ CCBCEntry }) => (
+      <ContentWrapper>
+        <h2>CCBC Entries</h2>
+        <GridThreeColumnContainer>
+          {CCBCEntry.edges.map(edge => (
+            <ProjectCard
+              youth={edge.node.frontmatter.youth}
+              semester={edge.node.frontmatter.semester}
+              title={edge.node.frontmatter.title}
+              key={edge.node.id}
+            />
+          ))}
+        </GridThreeColumnContainer>
+      </ContentWrapper>
+    )}
+  />
 );
 
-export default CCBCDigifabCohort;
+export default CCBCPage;
--- src/pages/courses/independent-study/index.js
+++ src/pages/courses/independent-study/index.js
@@ -1,10 +1,51 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { graphql, StaticQuery } from 'gatsby';
+import ProjectCard from '../../../../src/components/project-card';
+import {
+  ContentWrapper,
+  GridThreeColumnContainer,
+} from '../../../components/elements/layout-components';
 
-const IndependentStudy = () => (
-  <>
-    <h1>IND STUDY LANDING PAGE</h1>
-  </>
+const INDEPENDENT_STUDY_QUERY = graphql`
+  query IndependentStudyQuery {
+    IndependentStudyEntry: allMarkdownRemark(
+      filter: { frontmatter: { course: { eq: "independent-study" } } }
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            youth
+            semester
+          }
+        }
+      }
+    }
+  }
+`;
+
+// TODO: Make the project list more accessible by adding ul and li elements
+
+const IndependentStudyPage = () => (
+  <StaticQuery
+    query={INDEPENDENT_STUDY_QUERY}
+    render={({ IndependentStudyEntry }) => (
+      <ContentWrapper>
+        <h2>Independent Study Entries</h2>
+        <GridThreeColumnContainer>
+          {IndependentStudyEntry.edges.map(edge => (
+            <ProjectCard
+              youth={edge.node.frontmatter.youth}
+              semester={edge.node.frontmatter.semester}
+              title={edge.node.frontmatter.title}
+              key={edge.node.id}
+            />
+          ))}
+        </GridThreeColumnContainer>
+      </ContentWrapper>
+    )}
+  />
 );
 
-export default IndependentStudy;
+export default IndependentStudyPage;
