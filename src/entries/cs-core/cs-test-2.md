6fd423eaf4c1d8be970e9cd77f44aa0378ada19e
jonathanprozzi <jonathanprozzi@gmail.com>
2019-05-09 15:15:07 -0400

    finally got the filtered queries to work

---
+++ src/entries/cs-core/cs-test-2.md
@@ -0,0 +1,9 @@
+---
+youth: 'tracy'
+title: 'My CS Project'
+course: 'cs-core'
+semester: '2019spring'
+photo: '../../images/tester-card.jpg'
+---
+
+# HELLO CS
--- src/pages/courses/cs-core/index.js
+++ src/pages/courses/cs-core/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
-import { Link, graphql } from 'gatsby';
+import { graphql, StaticQuery } from 'gatsby';
 
-const CSCorePage = props => {
-  return (
-    <div>
-      <h1>HELLO FROM CS CORE PAGE</h1>
-    </div>
-  );
-};
+const CS_CORE_QUERY = graphql`
+  query CSCoreQuery {
+    allMarkdownRemark(filter: { frontmatter: { course: { eq: "cs-core" } } }) {
+      edges {
+        node {
+          frontmatter {
+            title
+          }
+        }
+      }
+    }
+  }
+`;
+
+const CSCorePage = () => (
+  <StaticQuery
+    query={CS_CORE_QUERY}
+    render={({ allMarkdownRemark }) => (
+      <div>
+        <h2>Entries</h2>
+        {allMarkdownRemark.edges.map(edge => (
+          <h3>{edge.node.frontmatter.title}</h3>
+        ))}
+      </div>
+    )}
+  />
+);
 
 export default CSCorePage;
