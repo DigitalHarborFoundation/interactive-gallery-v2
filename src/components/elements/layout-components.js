5b3d6c0fdad47ea79d4865f4eecaa6efe1077928
jonathanprozzi <jonathanprozzi@gmail.com>
2019-05-13 14:29:01 -0400

    tweaked the css for the project card further to be flexible while also still retaining certain style decisions

--- src/components/elements/layout-components.js
+++ src/components/elements/layout-components.js
@@ -67,6 +67,7 @@ export const ModalWrapper = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
+  background: rgba(0, 0, 0, 0.85);
 `;
 
 export const Container = styled.div`
--- src/templates/entry-template.js
+++ src/templates/entry-template.js
@@ -38,48 +38,53 @@ const EntryTemplate = props => {
     <>
       <GlobalStyle />
       <ModalWrapper>
-        <ModalCard>
+        <ProjectModalCard>
           <ProjectImageContainer>
-            <ProjectImage src={testImg} />
+            <ProjectImage src={jonathanTestImg} />
           </ProjectImageContainer>
-          <FlexColumnContainer marginAll="0" style={{ width: '80%' }}>
-            <h2
-              style={{
-                color: '#000',
-                fontSize: '3.2rem',
-                lineHeight: '2.4rem',
-                marginBottom: '0',
-                paddingBottom: '0',
-              }}
-            >
-              {capitalizeFirstLetter(Entries.frontmatter.title)}
-            </h2>
-            <FlexRowContainer marginAll="0">
-              <p style={{ color: '#5c5f5f', fontSize: '2rem' }}>
-                {capitalizeFirstLetter(Entries.frontmatter.youth)}
-              </p>
-              <Separator
-                color="#5c5f5f"
-                fontSize="2rem"
-                paddingLeft="1rem"
-                paddingRight="1rem"
+          <div style={{ width: '100%' }}>
+            <FlexColumnContainer marginAll="0" style={{ width: '100%' }}>
+              <h2
+                style={{
+                  color: '#000',
+                  fontSize: '3.2rem',
+                  lineHeight: '2.4rem',
+                  marginBottom: '0',
+                  paddingBottom: '0',
+                }}
+              >
+                {capitalizeFirstLetter(Entries.frontmatter.title)}
+              </h2>
+              <FlexRowContainer marginAll="0">
+                <p style={{ color: '#5c5f5f', fontSize: '2rem' }}>
+                  {capitalizeFirstLetter(Entries.frontmatter.youth)}
+                </p>
+                <Separator
+                  color="#5c5f5f"
+                  fontSize="2rem"
+                  paddingLeft="1rem"
+                  paddingRight="1rem"
+                />
+                <p style={{ color: '#5c5f5f', fontSize: '2rem' }}>
+                  {transformSemester(Entries.frontmatter.semester)}
+                </p>
+              </FlexRowContainer>
+              <p
+                style={{
+                  color: '#2d2a2a',
+                  fontSize: '2rem',
+                  lineHeight: '2.6rem',
+                  margin: '0 auto',
+                  paddingTop: '0',
+                  paddingBottom: '4rem',
+                  paddingLeft: '4rem',
+                  paddingRight: '4rem',
+                }}
+                dangerouslySetInnerHTML={{ __html: Entries.html }}
               />
-              <p style={{ color: '#5c5f5f', fontSize: '2rem' }}>
-                {transformSemester(Entries.frontmatter.semester)}
-              </p>
-            </FlexRowContainer>
-            <p
-              style={{
-                color: '#2d2a2a',
-                fontSize: '2rem',
-                lineHeight: '2.6rem',
-                margin: '0 auto',
-                padding: '4rem 4rem',
-              }}
-              dangerouslySetInnerHTML={{ __html: Entries.html }}
-            />
-          </FlexColumnContainer>
-        </ModalCard>
+            </FlexColumnContainer>
+          </div>
+        </ProjectModalCard>
       </ModalWrapper>
     </>
   );
@@ -87,17 +92,20 @@ const EntryTemplate = props => {
 
 export default EntryTemplate;
 
+const ProjectModalCard = styled(ModalCard)`
+  background: #ffffff;
+`;
+
 const ProjectImage = styled.img`
-  width: 100%;
-  height: auto;
-  min-height: 100%;
+  max-width: 100%;
+  margin: 4rem 0;
   display: block;
   object-fit: fill;
-  border-top-left-radius: 10px;
-  border-bottom-left-radius: 10px;
-  box-shadow: 4px 0px 10px rgba(0, 0, 0, 0.25);
+  border-radius: 10px;
+  box-shadow: 4px 8px 10px rgba(0, 0, 0, 0.25);
 `;
 
 const ProjectImageContainer = styled(ModalImageContainer)`
-  margin: 0 auto;
+  margin: auto;
+  padding-left: 4rem;
 `;
