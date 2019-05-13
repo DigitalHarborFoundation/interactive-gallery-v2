516e755b07c75d6a7e758d215a4d71e1869a378c
jonathanprozzi <jonathanprozzi@gmail.com>
2019-05-13 10:23:14 -0400

    added portal component with workaround for gatsby

---
+++ src/components/utilities/portal.js
@@ -0,0 +1,32 @@
+import { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+const portalRoot =
+  typeof document !== `undefined` ? document.getElementById('portal') : null;
+
+class Portal extends Component {
+  constructor() {
+    super();
+    this.el =
+      typeof document !== `undefined` ? document.createElement('div') : null;
+  }
+
+  componentDidMount = () => {
+    portalRoot.appendChild(this.el);
+  };
+
+  componentWillUnmount = () => {
+    portalRoot.removeChild(this.el);
+  };
+
+  render() {
+    const { children } = this.props;
+    if (this.el) {
+      return ReactDOM.createPortal(children, this.el);
+    } else {
+      return null;
+    }
+  }
+}
+
+export default Portal;
