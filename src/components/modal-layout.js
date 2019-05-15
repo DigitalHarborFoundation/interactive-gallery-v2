c87861e85f81b70a7c41bcc990477184614cb0c8
jonathanprozzi <jonathanprozzi@gmail.com>
2019-05-15 17:56:43 -0400

    escape closes the modal

--- src/components/modal-layout.js
+++ src/components/modal-layout.js
@@ -5,20 +5,6 @@ import { DialogOverlay, DialogContent } from '@reach/dialog';
 import VisuallyHidden from '@reach/visually-hidden';
 
 class ModalLayout extends Component {
-  componentDidMount() {
-    document.addEventListener('keydown', this.onKeyDown);
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.onKeyDown);
-  }
-
-  onKeyDown = ({ key }) => {
-    if (key === 'Escape') {
-      this.props.open && this.props.hideModal();
-    }
-  };
-
   disableScrolling(open) {
     if (open) {
       document.body.style.overflow = 'hidden';
@@ -29,10 +15,6 @@ class ModalLayout extends Component {
     }
   }
 
-  // if (typeof document !== 'undefined') {
-  //   this.disableScrolling(open);
-  // }
-
   render() {
     const { children, open, showModal, hideModal, referringPath } = this.props;
 
@@ -41,13 +23,20 @@ class ModalLayout extends Component {
         <DialogOverlay
           style={{ background: 'hsla(0, 100%, 100%, 0.9' }}
           isOpen={open}
+          onDismiss={() => {
+            push(`/courses/${referringPath}/`);
+          }}
         >
           <DialogContent
             style={{ boxShadow: '0px 10px 50px hsla(0, 0%, 0%, 0.33)' }}
           >
             <h1>HELLO</h1>
             <p>PATH: {referringPath}</p>
-            <button onClick={() => push(`/courses/${referringPath}/`)}>
+            <button
+              onClick={() => {
+                push(`/courses/${referringPath}/`);
+              }}
+            >
               CLOSE?
             </button>
             {children}
