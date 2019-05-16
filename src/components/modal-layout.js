0e4fd7c7450a625d239d1771d5dadccada1a1eba
jonathanprozzi <jonathanprozzi@gmail.com>
2019-05-15 23:44:43 -0400

    using previous modal styles

--- src/components/modal-layout.js
+++ src/components/modal-layout.js
@@ -48,12 +48,15 @@ class ModalLayout extends Component {
 export default ModalLayout;
 
 const StyledDialogOverlay = styled(DialogOverlay)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   position: fixed;
   top: 0;
   left: 0;
   right: 0;
   bottom: 0;
-  background: rgba(0, 0, 0, 0.75);
+  background: rgba(0, 0, 0, 0.85);
 `;
 
 const StyledDialogContent = styled(DialogContent)`
